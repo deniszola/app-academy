5d22313a062318248446db42ec360e6eacdb349b
Denis Batista de Andrade <34269902+deniszola@users.noreply.github.com>
2022-06-23 23:28:50 -0300

    feat: ✨ complete class practice

---
+++ 27_class/classes/person.js
@@ -0,0 +1,37 @@
+// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(
+      `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`
+    );
+  }
+
+  static introducePeople(people) {
+    if (!Array.isArray(people)) {
+      console.log("introducePeople only takes an array as an argument.");
+      return;
+    }
+
+    if (!people.every((person) => person instanceof Person)) {
+      console.log("All items in array must be Person class instances.");
+      return;
+    }
+
+    people.forEach((person) => person.introduce());
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Person;
+} catch {
+  module.exports = null;
+}
