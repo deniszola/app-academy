6e5ab26ae4e4122daf409bdd4f798f1f6ef63bb9
Denis Batista de Andrade <34269902+deniszola@users.noreply.github.com>
2022-07-06 18:20:13 +0000

    feat: ✨ complete context practice

---
+++ 39_context/problems/01-car-drive.js
@@ -0,0 +1,19 @@
+// Your code here
+class Car {
+  constructor() {
+    this.speed = 0;
+  }
+
+  drive(newSpeed) {
+    return (this.speed = newSpeed);
+  }
+}
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+  module.exports = Car;
+} catch {
+  module.exports = null;
+}
---
+++ 39_context/problems/06-fancy-calculator.js
@@ -0,0 +1,25 @@
+// Your code here
+const Calculator = require("./02-calculator");
+
+class FancyCalculator extends Calculator {
+  setTotal(newTotal) {
+    return (this.total = newTotal);
+  }
+
+  modulo(num) {
+    return (this.total %= num);
+  }
+
+  squared() {
+    return (this.total *= this.total);
+  }
+}
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+  module.exports = FancyCalculator;
+} catch {
+  module.exports = null;
+}
