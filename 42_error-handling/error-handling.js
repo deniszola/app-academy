412ec0a46e97dbdbde5b789dff9071ed4ccdd8dd
Denis Batista de Andrade <34269902+deniszola@users.noreply.github.com>
2022-07-09 11:36:01 +0000

    feat: ✨ complete error-handling practice

---
+++ 42_error-handling/error-handling.js
@@ -0,0 +1,52 @@
+// 1.
+function sum(array) {
+  try {
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+
+    return sum;
+  } catch (error) {
+    if (error instanceof TypeError) {
+      console.error(error.message);
+    }
+  }
+}
+
+let res = sum(null);
+console.log(res);
+
+// 2.
+// tests
+try {
+  sayName("Alex");
+  sayName(1);
+} catch (error) {
+  console.error(error.message);
+}
+
+// Your code here
+function sayName(name) {
+  if (typeof name !== "string") {
+    throw new TypeError("Invalid name! Must be a string!");
+  }
+
+  console.log(name);
+}
+
+// 3.
+function greet(greeting) {
+  if (!greeting) {
+    throw new Error("There was no greeting given.");
+  }
+
+  console.log(greeting);
+}
+
+try {
+  greet(null);
+} catch {
+  console.log("Hello World!");
+}
